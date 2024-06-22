c1fa186ff6611461d8a9b3d91438f0d8e9d00440
Sam Keen <sam.sjk@gmail.com>
2024-06-22 07:40:19 -0700

    up to 04_traits/14_outro

--- exercises/04_traits/11_clone/src/lib.rs
+++ exercises/04_traits/11_clone/src/lib.rs
@@ -2,9 +2,9 @@
 //  to get the code to compile.
 
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+    (ticket.clone(), ticket.summary())
 }
-
+#[derive(Clone)]
 pub struct Ticket {
     pub title: String,
     pub description: String,
@@ -19,7 +19,7 @@ impl Ticket {
         }
     }
 }
-
+#[derive(Clone)]
 pub struct Summary {
     pub title: String,
     pub status: String,
